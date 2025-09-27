d82316b83913743f18c97a322a95b499464776eb
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-09-27 06:17:05 +0530

    File updated at 9/27/2025, 6:17:04 AM

--- 1870-MinimumSpeedtoArriveonTime/1870-MinimumSpeedtoArriveonTime.java
+++ 1870-MinimumSpeedtoArriveonTime/1870-MinimumSpeedtoArriveonTime.java
@@ -1,39 +1,32 @@
-// Last updated: 9/27/2025, 6:07:02 AM
+// Last updated: 9/27/2025, 6:17:04 AM
 class Solution {
+    public boolean find(int t,int[] dict,double hour){
+        double tot=0;
+        for(int i=0;i<dict.length;i++){
+            if(i==dict.length-1) tot+=dict[i]/(double)t;
+            else if(  dict[i]%t!=0){
+                tot+=dict[i]/t+1;
+            }
+            else{
+                tot+=dict[i]/t;
+            }
+        }
+        System.out.println(tot);
+        return tot<=hour;
+    }
     public int minSpeedOnTime(int[] dist, double hour) {
-        return sol(dist,hour);
-    }
-    public static int sol(int arr[],double k){
-        int l=1;
-        int h=(int)1e7;
-        int ans=-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(ispossible(arr,mid,k)){
-                ans=mid;
-                h=mid-1;
+    int  n= dist.length;
 
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
+    int lo=1,hi=(int)1e7;
+    int ans=-1;
+    while(lo<=hi){
+        int mid=(hi-lo)/2+lo;
+       if(find(mid,dist,hour)){
+         ans =mid;
+         hi=mid-1;
+       }
+       else lo=mid+1;
     }
-    public static boolean ispossible(int arr[],int mid,double k){
-        double ti=0.0;
-        int n=arr.length;
-        for(int i=0;i<n;i++){
-            double t=(double)arr[i]/mid;
-            if(i!=n-1){
-                ti+=Math.ceil(t);
-            }
-            else{
-                ti+=t;
-            }
-            if(ti>k)return false;
-        }
-        return true;
-
+    return ans;
     }
 }
