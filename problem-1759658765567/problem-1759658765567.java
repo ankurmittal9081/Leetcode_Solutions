f2055f2503e684c46f7029b11b608835cc134c21
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-05 15:36:06 +0530

    Create

---
+++ problem-1759658765567/problem-1759658765567.java
@@ -0,0 +1,14 @@
+// Last updated: 10/5/2025, 3:36:05 PM
+class Solution {
+    public int maximum69Number (int num) {
+        String s=Integer.toString(num);
+        char arr[]=s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='6'){
+                arr[i]='9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(arr));
+    }
+}
