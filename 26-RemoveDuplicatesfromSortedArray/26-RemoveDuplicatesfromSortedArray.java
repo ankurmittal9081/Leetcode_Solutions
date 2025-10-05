5a345fa7b35399484295a133c46a20b73f633e17
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-05 13:08:02 +0530

    File updated at 10/5/2025, 1:08:01 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,17 +1,24 @@
-// Last updated: 10/5/2025, 1:01:01 PM
+// Last updated: 10/5/2025, 1:08:01 PM
 class Solution {
     public int removeDuplicates(int[] arr) {
-        int i=0;
-        int n=arr.length;
-        // int ans[]=new int[n];
-        for(int j=1;j<n;j++){
-            if(arr[j]!=arr[i]){
-                i++;
-                arr[i]=arr[j];
-                // i++;
+        // int i=0;
+        // int n=arr.length;
+        // for(int j=1;j<n;j++){
+        //     if(arr[j]!=arr[i]){
+        //         i++;
+        //         arr[i]=arr[j];
 
-            }
+        //     }
+        // }
+        // return i+1;
+        LinkedHashSet<Integer> set=new LinkedHashSet<>();
+        for(int i:arr){
+            set.add(i);
         }
-        return i+1;
+        int i=0;
+        for(int j:set){
+            arr[i++]=j;
+        }
+        return set.size();
     }
 }
