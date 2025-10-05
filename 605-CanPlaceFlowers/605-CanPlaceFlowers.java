32ce5d5bf0588753ee8b906330ab12ac98921619
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-05 15:01:25 +0530

    File updated at 10/5/2025, 3:01:25 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.java
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.java
@@ -1,43 +1,17 @@
-// Last updated: 10/5/2025, 2:55:05 PM
+// Last updated: 10/5/2025, 3:01:25 PM
 class Solution {
-    public boolean canPlaceFlowers(int[] fls, int m) {
-        int max=0;
-        int n=fls.length;
-        int fi=-1;
-        int li=-1;
-        for(int i=0;i<fls.length;i++){
-            if(fls[i]==0){
-                continue;
-            }
-
-            else if(fi==-1){
-                fi=i;
-                li=i;
-            }
-            else{
-                li=i;
-            }
-        }
-
-        if(fi==-1)  {
-            max=(n+1)/2;
-        }   
-        else{
-            int left=fi;
-            int right=n-1-li;
-            max=left/2;
-            max+=right/2;
-
-            int c=0;
-            for(int i=fi+1;i<=li-1;i++){
-                if(fls[i]==0) c++;
-                else {
-                    max+=(c-1)/2;
-                    c=0;
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i=0; i<flowerbed.length; i+=2){
+            if(flowerbed[i]==0){
+                if(i==flowerbed.length-1 || flowerbed[i+1] == 0){
+                    flowerbed[i] = 1;
+                    n--;
+                    if(n==0) return  true;
+                }else{
+                    i++;
                 }
             }
-            max+=(c-1)/2;    
         }
-        return m<=max;
+        return n<=0;
     }
 }
