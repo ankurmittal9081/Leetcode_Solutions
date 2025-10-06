1b9581ce5fe3cc1fb31d4ce6293c2a1ff41b08f3
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-06 21:47:29 +0530

    Create

---
+++ problem-1759767448073/problem-1759767448073.java
@@ -0,0 +1,23 @@
+// Last updated: 10/6/2025, 9:47:28 PM
+class Solution {
+    public int threeSumClosest(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n=arr.length;
+        int cl=arr[0]+arr[1]+arr[2];
+
+        for(int i=0;i<n-2;i++){
+            int l=i+1;
+            int r=n-1;
+
+            while(l<r){
+                int sum=arr[i]+arr[l]+arr[r];
+
+                if(Math.abs(sum-target)<Math.abs(cl-target)) cl=sum;
+                if(sum<target) l++;
+                else if(sum>target) r--;
+                else return sum;
+            }
+        }
+        return cl;
+    }
+}
