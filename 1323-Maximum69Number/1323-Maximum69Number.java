e30c4eeb3d157dbb73791b2170807df690fa5d46
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-06 10:43:31 +0530

    Create 1323-Maximum69Number

---
+++ 1323-Maximum69Number/1323-Maximum69Number.java
@@ -0,0 +1,13 @@
+// Last updated: 10/6/2025, 10:43:31 AM
+class Solution {
+    public int maximum69Number (int num) {
+        char arr[]=String.valueOf(num).toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]=='6') {
+                arr[i]='9';
+            return Integer.parseInt(new String(arr));
+            }
+        }
+        return num;
+    }
+}
