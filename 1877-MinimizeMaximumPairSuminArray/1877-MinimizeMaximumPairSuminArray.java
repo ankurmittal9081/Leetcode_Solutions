25283aa2351acd2164b9c48211c276189cad747e
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-06 22:29:28 +0530

    Create 1877-MinimizeMaximumPairSuminArray

---
+++ 1877-MinimizeMaximumPairSuminArray/1877-MinimizeMaximumPairSuminArray.java
@@ -0,0 +1,11 @@
+// Last updated: 10/6/2025, 10:29:27 PM
+class Solution {
+    public int minPairSum(int[] nums) {
+        int ans=0;
+        Arrays.sort(nums);
+        for(int i=0,j=nums.length-1;i<j;i++,j--){
+            ans=Math.max(ans,nums[i]+nums[j]);
+        }
+        return ans;
+    }
+}
