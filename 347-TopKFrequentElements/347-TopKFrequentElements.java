d33b90bf7b6fd9cc825939f675a8421ae13b5383
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-11 17:37:39 +0530

    File updated at 10/11/2025, 5:37:39 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,28 +1,21 @@
-// Last updated: 9/16/2025, 9:33:13 AM
+// Last updated: 10/11/2025, 5:37:39 PM
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] arr, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums){
+        for(int i:arr){
             map.put(i,map.getOrDefault(i,0)+1);
-
         }
-        PriorityQueue<Map.Entry<Integer,Integer>>pq=new PriorityQueue<>((a,b)->(a.getValue()-b.getValue()));
-            
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            pq.add(entry);
-            if(pq.size()>k){
-                pq.poll();
-            }
+        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
 
-
-        }        
-
-        int res[]=new int[k];
+        for(Map.Entry<Integer,Integer> mp:map.entrySet()){
+            pq.add(mp);
+            if(pq.size()>k) pq.poll();
+        }
+        int res[]=new int [k];
         int i=0;
         while(!pq.isEmpty()){
             res[i++]=pq.poll().getKey();
-            // i++;
         }
         return res;
     }
