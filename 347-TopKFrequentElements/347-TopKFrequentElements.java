1e2c766361ae630cf0470f17a6025df183fab6b6
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-11 18:00:29 +0530

    File updated at 10/11/2025, 6:00:29 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.java
+++ 347-TopKFrequentElements/347-TopKFrequentElements.java
@@ -1,22 +1,53 @@
-// Last updated: 10/11/2025, 5:37:39 PM
-class Solution {
-    public int[] topKFrequent(int[] arr, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:arr){
-            map.put(i,map.getOrDefault(i,0)+1);
+// Last updated: 10/11/2025, 6:00:29 PM
+public class Solution {
+    static {
+        int[] nums = {1, 1, 2, 2, 3};
+        for (int i = 0; i < 200; i++) {
+            topKFrequent(nums, 2);
         }
-
-        PriorityQueue<Map.Entry<Integer,Integer>> pq=new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
-
-        for(Map.Entry<Integer,Integer> mp:map.entrySet()){
-            pq.add(mp);
-            if(pq.size()>k) pq.poll();
+    }
+    public static int[] topKFrequent(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
         }
-        int res[]=new int [k];
-        int i=0;
-        while(!pq.isEmpty()){
-            res[i++]=pq.poll().getKey();
+        int[] freq = new int[max - min + 1];
+        int max_freq = 0;
+        for (int num : nums) {
+            int dif = num - min;
+            int f = ++freq[dif];
+            if (f > max_freq) {
+                max_freq = f;
+            }
         }
+        ArrayList<Integer>[] freqAr = new ArrayList[max_freq];
+        for (int i = 0; i < freq.length; i++) {
+            int n = freq[i] -1;
+            if (n == -1) {
+                continue;
+            }
+            if (freqAr[n] == null) {
+                freqAr[n] = new ArrayList<Integer>();
+            }
+            freqAr[n].add(i + min);
+        }
+        int[] res = new int[k];
+        int t = 0;
+        for (int i = max_freq - 1; i >= 0; i--) {
+            if (freqAr[i] == null) continue;
+            for (int j = 0; j < freqAr[i].size(); j++) {
+                res[t++] = freqAr[i].get(j);
+                if (t == k) {
+                    return res;
+                }
+            }
+        } 
         return res;
     }
 }
