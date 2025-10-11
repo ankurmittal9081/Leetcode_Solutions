c8dee06fb247e7837614c6ae1bbc15d80219ae12
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-11 15:21:45 +0530

    File updated at 10/11/2025, 3:21:44 PM

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.java
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -1,4 +1,4 @@
-// Last updated: 10/11/2025, 3:21:18 PM
+// Last updated: 10/11/2025, 3:21:44 PM
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         // for(int i=0;i<nums.length;i++){
@@ -9,25 +9,25 @@ class Solution {
         // }
         // return false;
 
-        // HashMap<Integer,Integer> map=new HashMap<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(map.containsKey(nums[i])){
-        //         int p=map.get(nums[i]);
-        //         if(i-p<=k) return true;
-        //     }
-        //     map.put(nums[i],i);
-        // }
-        // return false;
-
-        //using hashset+sliding windoww
-        HashSet<Integer> set=new HashSet<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(set.contains(nums[i])) return true;
-            set.add(nums[i]);
-            if(set.size()>k){
-                set.remove(nums[i-k]);
+            if(map.containsKey(nums[i])){
+                int p=map.get(nums[i]);
+                if(i-p<=k) return true;
             }
+            map.put(nums[i],i);
         }
         return false;
+
+        //using hashset+sliding windoww
+        // HashSet<Integer> set=new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(set.contains(nums[i])) return true;
+        //     set.add(nums[i]);
+        //     if(set.size()>k){
+        //         set.remove(nums[i-k]);
+        //     }
+        // }
+        // return false;
     }
 }
