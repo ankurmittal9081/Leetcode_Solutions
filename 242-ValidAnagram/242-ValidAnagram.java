f95f0472481835e1ad9416c1cec8cedefc8eabb1
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-11 15:37:35 +0530

    File updated at 10/11/2025, 3:37:34 PM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,17 +1,19 @@
-// Last updated: 10/11/2025, 3:35:44 PM
+// Last updated: 10/11/2025, 3:37:34 PM
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        char arr[]=s.toCharArray();
-        char arr1[]=t.toCharArray();
-        int n=arr.length;
-        int m=arr1.length;
-        Arrays.sort(arr);
-        Arrays.sort(arr1);
-        if(n!=m) return false;
-
-        for(int i=0;i<n;i++){
-            if(arr[i]!=arr1[i]) return false;
+    static{
+        for(int i=0;i<500;i++)
+            isAnagram("", "a");
         }
-        return true;
+    public static boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+       int[] count=new int[26];
+       for(int i=0;i<s.length();i++){
+        count[s.charAt(i)-'a']++;
+        count[t.charAt(i)-'a']--;
+       }
+       for(int i=0;i<26;i++){
+        if(count[i]!=0) return false;
+       }
+       return true;
     }
 }
