ec1dcedc91a915437e080565dbbb0ccac61c0ca3
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-12 16:46:51 +0530

    Create

---
+++ problem-1760267810821/problem-1760267810821.java
@@ -0,0 +1,29 @@
+// Last updated: 10/12/2025, 4:46:50 PM
+class Solution {
+    static {
+        int nums[]={1,2,3};
+        for(int i=0;i<=100;i++){
+            longestConsecutive(nums);
+        }
+    }
+    public static  int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        if(n<1) return 0;
+        Set<Integer> set=new TreeSet<>();
+        for(int i:nums) set.add(i);
+        List<Integer> l=new ArrayList<>(set);
+        // for(int i:l){
+        //     System.out.println(i+" ");
+        // }
+        int c=1;
+        int max=1;
+        for(int i=1;i<l.size();i++){
+            if(l.get(i)-l.get(i-1)==1) c++;
+            else c=1;
+            max=Math.max(max,c);
+        }
+
+        return max;
+
+    }
+}
