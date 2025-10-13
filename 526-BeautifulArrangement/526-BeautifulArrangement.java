9f3aa388e6c988cde0df012b54ad76f1e392c4b1
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-13 21:18:24 +0530

    File updated at 10/13/2025, 9:18:24 PM

--- 526-BeautifulArrangement/526-BeautifulArrangement.java
+++ 526-BeautifulArrangement/526-BeautifulArrangement.java
@@ -1,21 +1,20 @@
-// Last updated: 10/6/2025, 7:56:03 PM
+// Last updated: 10/13/2025, 9:18:24 PM
 class Solution {
     public int countArrangement(int n) {
         boolean arr[]=new boolean[n+1];
-        return solve(1,n,arr);  
+        return solve(1,n,arr);
     }
-
-    public static int solve(int pos,int n,boolean arr[]){
-        if(pos>n) return 1;
+    public int solve(int p,int n,boolean arr[]){
+        if(p>n) return 1;
         int c=0;
         for(int i=1;i<=n;i++){
-            if(!arr[i]&&(i%pos==0||pos%i==0)){
-                arr[i]=true;
-                c+=solve(pos+1,n,arr);
-                arr[i]=false;
-            }
+            if(!arr[i]&&(p%i==0||i%p==0)){
 
+            arr[i]=true;
+            c+=solve(p+1,n,arr);
+            arr[i]=false;
+            }
         }
-        return c;
+    return c;
     }
 }
