0a9fe7ac9b9e86f4c2c8a3d7a0b5f78c5f3f3de5
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-13 22:07:35 +0530

    File updated at 10/13/2025, 10:07:35 PM

--- 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.java
+++ 3403-FindtheLexicographicallyLargestStringFromtheBoxI/3403-FindtheLexicographicallyLargestStringFromtheBoxI.java
@@ -1,17 +1,17 @@
-// Last updated: 10/7/2025, 10:35:28 AM
+// Last updated: 10/13/2025, 10:07:35 PM
 class Solution {
     public String answerString(String s,int t) {
+        int n=s.length();
         String maxStr="";
         if(t==1){
             return s;
         }
-        int formed=s.length()-t+1;
-        for(int i=0;i<s.length();i++){
-            int canTakeLen=Math.min(formed,s.length()-i);
-            String newStr=s.substring(i,i+canTakeLen);
-            if(newStr.compareTo(maxStr)>0){
-                maxStr=newStr;
-            }
+        int c=n-(t-1);
+        for(int i=0;i<n;i++){
+
+            int end=Math.min(n,i+c);
+            String sub=s.substring(i,end);
+            if(sub.compareTo(maxStr)>0) maxStr=sub;
         }
         return maxStr;
         
