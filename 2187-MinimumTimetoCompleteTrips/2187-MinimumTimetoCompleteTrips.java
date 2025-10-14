d27f68354422f0f74a9397b07881a8bf17ca6a24
ankurmittal9081 <156496228+ankurmittal9081@users.noreply.github.com>
2025-10-14 06:10:57 +0530

    File updated at 10/14/2025, 6:10:56 AM

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,22 +1,17 @@
-// Last updated: 10/6/2025, 10:25:27 PM
+// Last updated: 10/14/2025, 6:10:56 AM
 class Solution {
     public long minimumTime(int[] arr, int tt) {
-         long l=1;
-         long h=(long)Arrays.stream(arr).min().getAsInt()*tt;
+        long l=1;
+        long h=(long) Arrays.stream(arr).min().getAsInt()*tt;
 
-         while(l<h){
+        long ans=0;
+        while(l<h){
             long mid=l+(h-l)/2;
-
             long sum=0;
-
-            for(int i:arr){
-                sum+=mid/i;
-            }
-            if(sum>=tt){
-                h=mid;
-            }
+            for(int i:arr) sum+=mid/i;
+            if(sum>=tt) h=mid;
             else l=mid+1;
-         }
-         return l;
+        }
+        return l;
     }
 }
